b3ae70d0604baeba7ddf114e6c40dfd75f6295e8
yanepumba <adress>
2023-10-16 20:48:33 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int enterInteger(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int searchFactorial(int number){
+    int i = 1;
+    int result = 1;
+    while(i <= number){
+        result *= i;
+        i++;
+    }
+    return result;
+}
+
+int number = enterInteger("Введите число: ");
+Console.WriteLine(searchFactorial(number));
